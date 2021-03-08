fba60b13f7e49f10774220521e002f51b129f34f
arbid2544 <mrarbid.m@ku.th>
2021-03-08 17:37:50 +0700

    Update 111_lab5-3.cpp

--- 111_lab5-3.cpp
+++ 111_lab5-3.cpp
@@ -1,14 +1,36 @@
 #include<stdio.h>
 int main()
 {
-	int a;
+	int a,b=0,c,k=0,i;
 	scanf("%d",&a);
-	for(int i=1;i<=a;i++)
+	for(i=1;k<a;i++)
 	{
-		if(i==1||i%2==0||i%3==0||i%5==0)
-		{
-			printf("%d",i);
-		}
+	    	c=i;
+	    	for(int j=0;j<=c;j++)
+			{
+			   if(c%2==0)
+			   {
+				c/=2;
+			   }
+			   else if(c%3==0)
+			   {
+			   	c/=3;
+			   }
+			   else if(c%5==0)
+			   {
+			   	c/=5;
+			   }
+	    	}
+	    	if(c==1)
+	    	{
+	    		//	printf("%d",i);
+	    			k++;
+			}
+			
+			
+			b++;
+		
 	}
+	printf(" %d",i-1);
 	return 0;
 }
